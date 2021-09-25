4afe93bf0854f5071d9362cfbd4bb4808d673c1e
vladeemerr <vdbakharev@mai.education>
2021-09-25 13:04:43 +0300

    Merge online and offline video streaming code

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ from streamlit_webrtc import (
     webrtc_streamer,
 )
 
+# NOTE: This is testing pseudo-DB, proper implementation requires *SQL
+# NOTE: To log in, type username 'bob' and password 'strawberry'  
 USERS = {'bob': hash('strawberry')}
 
 logger = logging.getLogger(__name__)
@@ -72,7 +74,8 @@ def download_file(url, download_to: Path, expected_size=None):
         if progress_bar is not None:
             progress_bar.empty()
 
-# Default ICE servers
+# NOTE: Default ICE servers
+# TODO: Is it possible to host default RTC/ICE server locally?
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
@@ -108,11 +111,7 @@ def main():
             st.stop()
 
     if st.session_state['login'] == True:
-        use_ext = st.checkbox('Use external file?', value=False)
-        if not use_ext: 
-            object_detection()
-        else:
-            object_detection_ext()
+        object_detection()
 
         # Log all living threads
         logger.debug("=== Alive threads ===")
@@ -155,6 +154,11 @@ def object_detection():
     download_file(PROTOTXT_URL, PROTOTXT_LOCAL_PATH, expected_size=29353)
 
     DEFAULT_CONFIDENCE_THRESHOLD = 0.5
+
+    _ext_file = str('')
+
+    def player():
+        return MediaPlayer(str(Path(__file__).parent / f"data/{_ext_file.name}"))
 
     class Detection(NamedTuple):
         name: str
@@ -226,14 +230,27 @@ def object_detection():
             return av.VideoFrame.from_ndarray(annotated_image, format="bgr24")
 
     # NOTE: Initialize WebRTC context
-    webrtc_ctx = webrtc_streamer(
-        key="detection", # NOTE: Any name
-        mode=WebRtcMode.SENDRECV, # NOTE: Has to be `SENDRECV` to allow frame processing
-        rtc_configuration=RTC_CONFIGURATION, # NOTE: Give a list of known ICE servers
-        video_processor_factory=Processor, # NOTE: Set processor class (should have `recv(frame)`)
-        async_processing=True, # NOTE: Async enabled
-        media_stream_constraints={"video": True}, # NOTE: Only allow video stream
-    )
+    webrtc_ctx = None
+    if not st.checkbox('Use external file?', value=False):
+        webrtc_ctx = webrtc_streamer(
+            key='detection', # NOTE: Any name
+            mode=WebRtcMode.SENDRECV, # NOTE: Has to be `SENDRECV` to allow frame processing
+            rtc_configuration=RTC_CONFIGURATION, # NOTE: Give a list of known ICE servers
+            video_processor_factory=Processor, # NOTE: Set processor class (should have `recv(frame)`)
+            async_processing=True, # NOTE: Async enabled
+            media_stream_constraints={"video": True}, # NOTE: Only allow video stream
+        )
+    else:
+        _ext_file = st.file_uploader('Choose video file')
+
+        webrtc_ctx = webrtc_streamer(
+            key="detection-via-external",
+            mode=WebRtcMode.RECVONLY,
+            rtc_configuration=RTC_CONFIGURATION,
+            media_stream_constraints={"video": True},
+            player_factory=player,
+            video_processor_factory=Processor,
+        )
 
     # NOTE: Put annotations in a UI table as well
     if webrtc_ctx.state.playing:
@@ -255,138 +272,6 @@ def object_detection():
                 labels.table(result)
             else:
                 break
-
-def object_detection_ext():
-    ext_file = st.file_uploader('Choose video file')
-    if ext_file is not None:
-        st.write(ext_file)
-
-    MODEL_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.caffemodel"  # noqa: E501
-    MODEL_LOCAL_PATH = Path(__file__).parent / "./models/MobileNetSSD_deploy.caffemodel"
-    PROTOTXT_URL = "https://github.com/robmarkcole/object-detection-app/raw/master/model/MobileNetSSD_deploy.prototxt.txt"  # noqa: E501
-    PROTOTXT_LOCAL_PATH = Path(__file__).parent / "./models/MobileNetSSD_deploy.prototxt.txt"
-
-    CLASSES = [
-        "background",
-        "aeroplane",
-        "bicycle",
-        "bird",
-        "boat",
-        "bottle",
-        "bus",
-        "car",
-        "cat",
-        "chair",
-        "cow",
-        "diningtable",
-        "dog",
-        "horse",
-        "motorbike",
-        "person",
-        "pottedplant",
-        "sheep",
-        "sofa",
-        "train",
-        "tvmonitor",
-    ]
-    COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
-
-    download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=23147564)
-    download_file(PROTOTXT_URL, PROTOTXT_LOCAL_PATH, expected_size=29353)
-
-    DEFAULT_CONFIDENCE_THRESHOLD = 0.5
-
-    st.write()
-
-    class Detection(NamedTuple):
-        name: str
-        prob: float
-
-    def player():
-        return MediaPlayer(str(Path(__file__).parent / f"data/{ext_file.name}"))
-
-    class DetectionProcessor(VideoProcessorBase):
-        confidence_threshold: float
-        result_queue: "queue.Queue[List[Detection]]"
-
-        def __init__(self) -> None:
-            # Initialize detection model (proto and model)
-            self._net = cv2.dnn.readNetFromCaffe(
-                str(PROTOTXT_LOCAL_PATH), str(MODEL_LOCAL_PATH)
-            )
-            self.confidence_threshold = DEFAULT_CONFIDENCE_THRESHOLD
-            self.result_queue = queue.Queue()
-
-        def _annotate_image(self, image, detections):
-            # NOTE: Iterate over the detections
-            (h, w) = image.shape[:2]
-            result: List[Detection] = []
-            for i in np.arange(0, detections.shape[2]):
-                confidence = detections[0, 0, i, 2]
-
-                if confidence > self.confidence_threshold:
-                    # NOTE: extract the index of the class label from the `detections`,
-                    # then compute the (x, y)-coordinates of the bounding box for
-                    # the object
-                    idx = int(detections[0, 0, i, 1])
-                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                    (startX, startY, endX, endY) = box.astype("int")
-
-                    name = CLASSES[idx]
-                    result.append(Detection(name=name, prob=float(confidence)))
-
-                    # NOTE: Display the prediction
-                    label = f"{name}: {round(confidence * 100, 2)}%"
-                    cv2.rectangle(image, (startX, startY), (endX, endY), COLORS[idx], 2)
-                    y = startY - 15 if startY - 15 > 15 else startY + 15
-                    cv2.putText(
-                        image,
-                        label,
-                        (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5,
-                        COLORS[idx],
-                        2,
-                    )
-            return image, result
-
-        # NOTE: On recv do annotation on a video frame
-        def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-            image = frame.to_ndarray(format="bgr24") # NOTE: Allocate frame buffer
-            blob = cv2.dnn.blobFromImage( # NOTE: Create handle for frame processing
-                cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
-            )
-            self._net.setInput(blob) # NOTE: Bind net to the handle
-            detections = self._net.forward() # NOTE: Get detections
-
-            # NOTE: Get annotated video frame and data pairs list
-            annotated_image, result = self._annotate_image(image, detections)
-
-            # NOTE: Put results in a queue for further use
-            # XXX: Current method is called in another thread,
-            # so it must be thread-safe.
-            self.result_queue.put(result)
-
-            # NOTE: Finally, return processed frame
-            return av.VideoFrame.from_ndarray(annotated_image, format="bgr24")
-
-    class Processor(VideoProcessorBase):
-        def __init__(self) -> None:
-            pass
-
-        def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-            img = frame.to_ndarray(format="bgr24")
-
-            return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-    webrtc_ctx = webrtc_streamer(
-        key="detection-via-external",
-        mode=WebRtcMode.RECVONLY,
-        rtc_configuration=RTC_CONFIGURATION,
-        media_stream_constraints={"video": True},
-        player_factory=player,
-        video_processor_factory=DetectionProcessor,
-    )
 
 if __name__ == "__main__":
     import os
